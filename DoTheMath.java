5c71ef394a2e08ac7b10a3d959b7dcb60ddb8f4b
MrsHines <34042849+MrsHines@users.noreply.github.com>
2018-08-15 14:13:15 -0500

    Assignment 2

---
+++ DoTheMath.java
@@ -0,0 +1,33 @@
+/**
+ * Write a desctiption of class DoTheMath here.
+ *
+ * @author __________
+ * @date   __________
+ */
+ 
+ public class DoTheMath 
+ {
+      public static void main(String[] args)
+      {
+          //define two variables
+          //create a Scanner
+          //take in the input for two variables
+          //call the methods
+      }
+      
+      /**
+       * Add the variables and print the sum
+       *
+       * @param - two ints
+       */
+       
+       public void sumThem(int numone, int numTwo)
+       {
+           // find the sum
+       }
+       
+       // Add the methods and documentation for subtracting, multiplying, and dividing
+       
+ }
+       
+          
