0bea48d123ae098fdd24a61f44bdc28f66860aee
MrsHines <34042849+MrsHines@users.noreply.github.com>
2018-08-15 14:23:15 -0500

    Assignment #3

---
+++ Rectangle.java
@@ -0,0 +1,29 @@
+/**
+ * Write a description of the class Rectangle
+ *
+ * @author ___________
+ * @date ___________
+ *
+ */
+ 
+ public class Rectangle
+ {
+    public static void main(String[] args)
+    {
+        // declare variables for the length and width of the rectangle
+        // create a Scanner and ask the user for the values
+        
+        /**
+         * Determine the perimeter of the rectangle
+         * @param - 2 ints
+         *
+         */
+         
+         public void perimeter(int wide, int tall)
+         {
+            //Calculate and print the perimeter of the rectangle
+         }
+         
+         //Create a method for finding the area of the rectangle
+      }
+ }
