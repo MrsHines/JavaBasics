14aad32589d5779a84526d0dce91a3b5b5bd6198
MrsHines <34042849+MrsHines@users.noreply.github.com>
2018-08-21 10:39:51 -0500

    Fixed curly bracket

--- Rectangle.java
+++ Rectangle.java
@@ -12,18 +12,19 @@
     {
         // declare variables for the length and width of the rectangle
         // create a Scanner and ask the user for the values
-        
+    }
+  
         /**
          * Determine the perimeter of the rectangle
          * @param - 2 ints
          *
          */
-         
-         public stitic void perimeter(int wide, int tall)
-         {
-            //Calculate and print the perimeter of the rectangle
-         }
+    
+    public static void perimeter(int wide, int tall)
+    {
+        //Calculate and print the perimeter of the rectangle
+    }
          
          //Create a method for finding the area of the rectangle
-      }
+      
  }
